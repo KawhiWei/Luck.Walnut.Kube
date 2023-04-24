4d3a7c541f897996c1c9ef076878a3bd8881ba31
白蛇 <1162321341@qq.com>
2023-04-24 18:54:09 +0800

    提交k8s

---
+++ src/Luck.Walnut.Kube.Adapter/KubernetesAdapter/NameSpaces/NameSpaces.cs
@@ -0,0 +1,35 @@
+﻿using k8s;
+using k8s.Models;
+
+using Luck.Framework.Infrastructure.DependencyInjectionModule;
+using Luck.Walnut.Kube.Adapter.Factories;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luck.Walnut.Kube.Adapter.KubernetesAdapter.NameSpaces
+{
+    public class NameSpaces : INameSpaces
+    {
+        private readonly IKubernetesClientFactory _kubernetesClientFactory;
+
+        public Task CreateNameSpaceAsync(string config, V1Namespace v1Namespace)
+        {
+            var client = _kubernetesClientFactory.GetKubernetesClient(config);
+            client.CoreV1.CreateNamespaceAsync(v1Namespace);
+
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface INameSpaces : IScopedDependency
+    {
+        Task CreateNameSpaceAsync(string config,V1Namespace v1Namespace);
+
+
+    }
+}
